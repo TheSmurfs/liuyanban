a86ae1fc7ca88564e357fd47ad656a0708863ffe
TheSmurfs <752105005@qq.com>
2020-02-19 14:11:58 +0800

    完结

--- app/message_form/views.py
+++ app/message_form/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from app.message_form.models import Message
 
 
 def message_form(request):
     return render(request, "message_form.html")
 
+
 # Create your views here.
+
+
+def message_from(request):
+    all_messages = Message.objects
+    for message in all_messages:
+        print(message.name)
+    return render(request, "message_form.html")
